e557fda3674de04ee67e3529a64200ed759be227
Sushil Singh <sushils@vmware.com>
2022-03-15 16:58:48 -0700

    some more info in function
    Signed-off-by: Sushil Singh <sushils@vmware.com>

--- my-functions/hello.js
+++ my-functions/hello.js
@@ -1,6 +1,9 @@
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello World from example." }),
+      body: JSON.stringify({ message: `Hello World from example. \n
+        path=${event.path}, headers=${event.headers}\n
+        clientcontext=${context.clientContex}
+        ` }),
     };
   }
