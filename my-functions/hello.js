69f8462a9b556f380acd6d4c7e35558fcc0a8347
Sushil Singh <sushils@vmware.com>
2022-03-15 17:36:21 -0700

    bump
    Signed-off-by: Sushil Singh <sushils@vmware.com>

--- my-functions/hello.js
+++ my-functions/hello.js
@@ -1,7 +1,7 @@
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello World from example. \n
+      body: JSON.stringify({ message: `Hello World from example v3. \n
         path=${event.path}, headers=${event.headers}\n
         clientcontext=${context.clientContex}
         ` }),
